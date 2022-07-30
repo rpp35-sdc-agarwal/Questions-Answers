d26c35b1e3b0822409b62daed51d6b2ff77af089
Xiuqi Cai <cauxiuqi2013@gmail.com>
2022-07-30 15:05:09 -0700

    Created database instance on ec2, and connected successfully

--- reviews-API/database/index.js
+++ reviews-API/database/index.js
@@ -1,5 +1,9 @@
 const Pool = require('pg').Pool;
 
+///////////////////////////////
+// LOCAL DATABASE CONNECTION //
+///////////////////////////////
+
 // const pool = new Pool({
 //   user: 'caixiuqi',
 //   password: 'PASSWORD',
@@ -8,6 +12,10 @@ const Pool = require('pg').Pool;
 //   port: 5432
 // })
 
+/////////////////////////////////
+// AWS RDS DATABASE CONNECTION //
+/////////////////////////////////
+
 const pool = new Pool({
   user: 'postgres',
   password: 'PASSWORD',
@@ -15,5 +23,17 @@ const pool = new Pool({
   host: 'sdc-reviews-postgres.cnq3t56crzzg.us-west-2.rds.amazonaws.com',
   port: 5432
 })
+
+/////////////////////////////////
+// AWS EC2 DATABASE CONNECTION //
+/////////////////////////////////
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'PASSWORD',
+//   database: 'postgres',
+//   host: '54.218.163.60',
+//   port: 5432
+// })
 
 module.exports = pool;
